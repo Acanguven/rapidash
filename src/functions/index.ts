5e1811e849385318405f6732cf3438c4cd5b27e4
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2020-02-03 22:42:45 +0300

    Added avarage

---
+++ src/functions/index.ts
@@ -0,0 +1,5 @@
+import { solution as max } from './max';
+import { solution as factorial } from './factorial';
+import { solution as average } from './average';
+
+export { max, factorial, average };
