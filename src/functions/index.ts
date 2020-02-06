ca2cbc6645cda314ee90b5d1800ec7377db8fd0c
yavuzkoca <ykoca0808@gmail.com>
2020-02-06 09:31:38 +0300

    Refactor: Change palindrome filename

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,7 +2,7 @@ import { solution as max } from './max';
 import { solution as factorial } from './factorial';
 import { solution as fibonacci } from './fibonacci';
 import { solution as average } from './average';
-import { solution as isPalindrome } from './isPalindrome';
+import { solution as isPalindrome } from './is-palindrome';
 import { solution as primes } from './primes';
 import { solution as times } from './times';
 
