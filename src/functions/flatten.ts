743b2e92b787c7387f31a8f43fe84b85c32f4123
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-02-06 12:35:17 +0300

    Fix: function relocation

--- src/functions/flatten.ts
+++ src/functions/flatten.ts
@@ -17,34 +17,32 @@ solution
     [1, 3, 4, 5]
   )
   .bench([[1, 2, [2, 3, 4, [3, 4, [5]]]], 1]);
-
-const isFlattenable = (value: any) => {
-  return Array.isArray(value) || !!(value && value[Symbol.isConcatSpreadable]);
-};
-const base = (array: any, depth: number, result: any[]): any => {
-  if (array === null || array === undefined) {
-    return result;
-  }
-
-  for (let index = 0; index < array.length; index++) {
-    const value = array[index];
-    if (depth > 0 && isFlattenable(value)) {
-      if (depth > 1) {
-        base(value, depth - 1, result);
-      } else {
-        result.push(...(value as []));
-      }
-    } else {
-      result[result.length] = value;
-    }
-  }
-
-  return result;
-};
-const flatten = <T>(array: T[], depth = 1): T[] => {
-  return base(array, depth, []);
-};
 solution
   .owner('mehmetsefabalik')
   .method('Flatten')
-  .fn(flatten);
+  .fn(<T>(array: T[], depth = 1): T[] => {
+    const isFlattenable = (value: any) => {
+      return Array.isArray(value) || !!(value && value[Symbol.isConcatSpreadable]);
+    };
+    const base = (array: any, depth: number, result: any[]): any => {
+      if (array === null || array === undefined) {
+        return result;
+      }
+
+      for (let index = 0; index < array.length; index++) {
+        const value = array[index];
+        if (depth > 0 && isFlattenable(value)) {
+          if (depth > 1) {
+            base(value, depth - 1, result);
+          } else {
+            result.push(...(value as []));
+          }
+        } else {
+          result[result.length] = value;
+        }
+      }
+
+      return result;
+    };
+    return base(array, depth, []);
+  });
